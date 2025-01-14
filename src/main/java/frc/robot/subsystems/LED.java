19039175970bbcb085de6c560191fa36a1077f35
Aiden Powers <powersajbusiness@protonmail.com>
2025-01-14 15:55:54 -0800

    clean up code formatting

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -171,34 +171,26 @@ public class LED extends SubsystemBase {
 
   // Setters
 
-  /**
-   * setSolidColor() - The LEDs will show a static color.
-   */
+  /** setSolidColor() - The LEDs will show a static color. */
   private void setSolidColor(Color color) {
     LEDPattern solid = LEDPattern.solid(color);
     solid.applyTo(ledBuffer);
     led.setData(ledBuffer);
   }
 
-  /**
-   * setProgressBar() - A bottom-up progress bar will fill the LEDs with pure white at %100.
-   */
+  /** setProgressBar() - A bottom-up progress bar will fill the LEDs with pure white at %100. */
   private void setProgressBar(Color color, double percent) {
     LEDPattern progress = LEDPattern.progressMaskLayer(() -> (percent) / 100);
     progress.applyTo(ledBuffer);
     led.setData(ledBuffer);
   }
 
-  /**
-   * setBlinking() - The LEDs will blink a color at the predefined interval of 0.1s
-   */
+  /** setBlinking() - The LEDs will blink a color at the predefined interval of 0.1s */
   private void setBlinking(Color color) {
     setBlinking(color, 0.1);
   }
 
-  /**
-   * setBlinking() - The LEDs will blink a color at a specific interval
-   */
+  /** setBlinking() - The LEDs will blink a color at a specific interval */
   private void setBlinking(Color color, double seconds) {
     LEDPattern solid = LEDPattern.solid(color);
     LEDPattern blink = solid.blink(Seconds.of(seconds));
@@ -206,18 +198,16 @@ public class LED extends SubsystemBase {
     led.setData(ledBuffer);
   }
 
-  /**
-   * setSnake() - Gradient of two colors that will scroll across the LEDs continously
-   */
+  /** setSnake() - Gradient of two colors that will scroll across the LEDs continously */
   private void setSnake(Color color1, Color color2) {
-    LEDPattern gradient = LEDPattern.gradient(LEDPattern.GradientType.kContinuous, color1, color2).scrollAtAbsoluteSpeed(Centimeters.per(Second).of(12.5), ledSpacing);
+    LEDPattern gradient =
+        LEDPattern.gradient(LEDPattern.GradientType.kContinuous, color1, color2)
+            .scrollAtAbsoluteSpeed(Centimeters.per(Second).of(12.5), ledSpacing);
     gradient.applyTo(ledBuffer);
     led.setData(ledBuffer);
   }
 
-  /**
-   * setRainbow() - Rainbow that will scroll across the LEDs continously
-   */
+  /** setRainbow() - Rainbow that will scroll across the LEDs continously */
   private void setRainbow() {
     LEDPattern rainbow = LEDPattern.rainbow(255, 128);
     LEDPattern scrollingRainbow = rainbow.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), ledSpacing);
@@ -225,9 +215,7 @@ public class LED extends SubsystemBase {
     led.setData(ledBuffer);
   }
 
-  /**
-   * setRobotState() - Set the LED robotState.
-   */
+  /** setRobotState() - Set the LED robotState. */
   public void setRobotState(RobotState robotState) {
     this.robotState = robotState;
   }
@@ -240,43 +228,45 @@ public class LED extends SubsystemBase {
   @SuppressWarnings("unused")
   private void setAudioLevelMeter(int bpm) {
     double theta = levelMeterCount * 0.02 * Math.PI * bpm / 60.0;
-    double volume = 1 + Math.abs(11.0 * Math.sin(theta)) + (3.0 * Math.sin(theta * 7.0)) + (1.0 * Math.sin(theta * 17.0));
+    double volume =
+        1
+            + Math.abs(11.0 * Math.sin(theta))
+            + (3.0 * Math.sin(theta * 7.0))
+            + (1.0 * Math.sin(theta * 17.0));
     double mappedVolume = (volume / 15) * 100;
     LEDPattern volumeMask = LEDPattern.progressMaskLayer(() -> (mappedVolume) / 100);
-    LEDPattern volumeGradient = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, Color.kGreen, Color.kRed).mask(volumeMask);
+    LEDPattern volumeGradient =
+        LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, Color.kGreen, Color.kRed)
+            .mask(volumeMask);
     volumeGradient.applyTo(ledBuffer);
     led.setData(ledBuffer);
     levelMeterCount += 1;
   }
 
-  /**
-   * setSeaLevelGraphic() - Looks like the side view of a changing sea level
-   */
+  /** setSeaLevelGraphic() - Looks like the side view of a changing sea level */
   private void setSeaLevelGraphic() {
-    double seaLevel = 2 + Math.sin(levelMeterCount*.01) + Math.sin(levelMeterCount*.02);
+    double seaLevel = 2 + Math.sin(levelMeterCount * .01) + Math.sin(levelMeterCount * .02);
     double mappedSeaLevel = (seaLevel / 4) * 100;
     LEDPattern seaMask = LEDPattern.progressMaskLayer(() -> (mappedSeaLevel) / 100);
     Color deepSea = new Color(0.0, 0.01, 0.025);
     Color shallowSea = new Color(0.0, 0.3, 0.4);
-    Color seaColor = Color.lerpRGB(shallowSea, deepSea, mappedSeaLevel/100);
-    LEDPattern seaGradient = LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, deepSea, seaColor).mask(seaMask);
+    Color seaColor = Color.lerpRGB(shallowSea, deepSea, mappedSeaLevel / 100);
+    LEDPattern seaGradient =
+        LEDPattern.gradient(LEDPattern.GradientType.kDiscontinuous, deepSea, seaColor)
+            .mask(seaMask);
     seaGradient.applyTo(ledBuffer);
     led.setData(ledBuffer);
     levelMeterCount += 1;
   }
 
-  /**
-   * setBaseRobotState() - Needs an educated description.
-   */
-  //FIXME: Needs an educated description.
+  /** setBaseRobotState() - Needs an educated description. */
+  // FIXME: Needs an educated description.
   public void setBaseRobotState(BaseRobotState baseRobotState) {
     this.baseRobotState = baseRobotState;
   }
 
-  /**
-   * setGamepieceStatus() - Needs an educated description.
-   */
-  //FIXME: Needs an educated description.
+  /** setGamepieceStatus() - Needs an educated description. */
+  // FIXME: Needs an educated description.
   public void setGamepieceStatus(boolean gamepieceInRobot) {
     this.gamepieceInRobot = gamepieceInRobot;
   }
