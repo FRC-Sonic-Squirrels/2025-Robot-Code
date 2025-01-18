5ee9a06eb86bb560285b9244e80e6b39f054455a
PsiFi96 <lt.theaker@icloud.com>
2025-01-17 20:44:40 -0800

    added basic intake logic to IntakeGamepiece

--- src/main/java/frc/robot/commands/intake/IntakeGamepieceCoralStation.java
+++ src/main/java/frc/robot/commands/intake/IntakeGamepieceCoralStation.java
@@ -18,17 +18,13 @@ public class IntakeGamepieceCoralStation extends Command {
   private static final LoggedTunableNumber intakingVelocity = group.build("intakingVelocity", 2500);
   private final Intake intake;
   private final EndEffector endEffector;
-  private final Boolean intakeGround;
 
   /** Creates a new IntakeDefaultIdleRPM. */
   public IntakeGamepieceCoralStation(Intake intake, EndEffector endEffector) {
     // TODO: add subsystems
     this.intake = intake;
     this.endEffector = endEffector;
-    this.intakeGround = intakeGround;
     // TODO: add subsystem requirements
-    addRequirements(intake);
-    addRequirements(intake);
     addRequirements(intake);
     setName("IntakeGamepiece");
   }
@@ -63,6 +59,8 @@ public class IntakeGamepieceCoralStation extends Command {
   @Override
   public void end(boolean interrupted) {
     intake.setRollerVelocity(0.0);
+    endEffector.setVelocity(0.0);
+    intake.setVelocity(0.0);
     endEffector.setVelocity(0.0);
   }
 
