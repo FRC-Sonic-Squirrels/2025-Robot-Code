26ab906ac79faf5420bcd3e52e37ed8aad95e0ec
PsiFi96 <lt.theaker@icloud.com>
2025-01-20 16:52:19 -0800

    added basic tof logic to IntakeGamepiece

--- src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffector.java
@@ -145,4 +145,8 @@ public class EndEffector extends SubsystemBase {
   public boolean tofSeenGamepiece() {
     return inputs.tofDistInches <= distanceToTriggerCoralDetection.get();
   }
+
+  public boolean tofSeenGamepiece() {
+    return tofDistance().magnitude() <= distanceToTriggerCoralDetection.get();
+  }
 }
