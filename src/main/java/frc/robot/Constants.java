a5f80fae1aec9f0279d84aeebaf2df4cf3bc9711
Runner112233 <viljamiolder@outlook.com>
2025-01-24 19:27:18 -0800

    Adjusted the Constants to fit the Pivot better.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -233,22 +233,23 @@ public final class Constants {
     public static final String ROOT_TABLE = "Intake";
   }
 
-  public static class IntakePivotConstants { // TODO: get the real values of all of these
+  public static class IntakePivotConstants { // TODO: check all constants
     public static final double SUPPLY_CURRENT_LIMIT = 0;
 
-    public static final double GEAR_RATIO = 1;
+    public static final double GEAR_RATIO = (16 / 42) * (16 / 56);
 
     public static final double MOI = 0;
 
-    public static final Rotation2d MAX_PIVOT_ANGLE = Rotation2d.fromDegrees(0);
+    public static final Rotation2d MAX_PIVOT_ANGLE = Rotation2d.fromDegrees(180);
     public static final Rotation2d MIN_PIVOT_ANGLE = Rotation2d.fromDegrees(0);
     public static final Rotation2d HOME_POSITION = MIN_PIVOT_ANGLE;
 
-    public static final Rotation2d PIVOT_SAFE_ANGLE = Rotation2d.fromDegrees(0);
+    public static final Rotation2d PIVOT_SAFE_ANGLE = Rotation2d.fromDegrees(90);
 
-    public static final Rotation2d TRAP_SCORE_ANGLE = Rotation2d.fromDegrees(0);
+    public static final Rotation2d INTERNAL_ANGLE = Rotation2d.fromDegrees(0);
+    public static final Rotation2d EXTERNAL_ANGLE = Rotation2d.fromDegrees(180);
 
-    public static final Distance PIVOT_LENGTH = Units.Inches.of(0);
+    public static final Distance PIVOT_LENGTH = Units.Inches.of(6.215);
 
     public static final String ROOT_TABLE = "IntakePivot";
   }
