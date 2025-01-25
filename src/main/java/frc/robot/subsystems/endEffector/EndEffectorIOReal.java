49c63c59846468e2b9a9f9816b3ba87408789115
Tim <timellis337@gmail.com>
2025-01-25 14:56:58 -0800

    note being scored

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOReal.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOReal.java
@@ -39,7 +39,7 @@ public class EndEffectorIOReal implements EndEffectorIO {
 
   private final BaseStatusSignal[] refreshSet;
 
-  private final TimeOfFlight tof = new TimeOfFlight(Constants.CanIDs.ELEVATOR_CAN_ID);
+  private final TimeOfFlight tof = new TimeOfFlight(Constants.CanIDs.END_EFFECTOR_TOF_CAN_ID);
 
   public EndEffectorIOReal() {
     // Motor config
