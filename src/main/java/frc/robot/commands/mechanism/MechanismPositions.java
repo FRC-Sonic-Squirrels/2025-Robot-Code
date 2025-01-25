07c19d93786ae4c5bb0c6675d9911f9eb5d6989b
Tim <timellis337@gmail.com>
2025-01-25 14:56:58 -0800

    note being scored

--- src/main/java/frc/robot/commands/mechanism/MechanismPositions.java
+++ src/main/java/frc/robot/commands/mechanism/MechanismPositions.java
@@ -84,7 +84,7 @@ public class MechanismPositions {
       reefL3ArmAngleDegrees.initDefault(0);
       reefL4ElevatorHeightInches.initDefault(26.2);
       reefL4ArmAngleDegrees.initDefault(120);
-      coralStationElevatorHeightInches.initDefault(16);
+      coralStationElevatorHeightInches.initDefault(25);
       coralStationArmAngleDegrees.initDefault(-45);
       algaeClearingLow1ElevatorHeightInches.initDefault(16);
       algaeClearingLow1ArmAngleDegrees.initDefault(120);
