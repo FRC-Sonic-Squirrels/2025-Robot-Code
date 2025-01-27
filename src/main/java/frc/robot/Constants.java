4f3980042648d10f9492ba91d7b4adc4ac2917e8
Tim <timellis337@gmail.com>
2025-01-27 13:22:20 -0800

    change robot type back to Maestro for testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -56,7 +56,7 @@ public final class Constants {
   }
 
   public static class RobotMode {
-    private static final RobotType ROBOT = RobotType.ROBOT_2025;
+    private static final RobotType ROBOT = RobotType.ROBOT_2024_RETIRED_MAESTRO;
 
     private static final Alert invalidRobotAlert =
         new Alert("Invalid robot selected, using competition robot as default.", AlertType.ERROR);
