724ae9e58cdbf7f18eb42dc86c1df9c974779c54
PsiFi96 <lt.theaker@icloud.com>
2025-01-27 16:29:56 -0800

    changes after the rebase

--- src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOReal.java
+++ src/main/java/frc/robot/subsystems/endEffector/EndEffectorIOReal.java
@@ -60,11 +60,6 @@ public class EndEffectorIOReal implements EndEffectorIO {
 
     motor.getConfigurator().apply(config);
 
-    // TODO: update values to reflect actual robot
-    tof.setRangeOfInterest(6, 6, 10, 10);
-
-    tof.setRangingMode(RangingMode.Short, 25);
-
     // Status signals
 
     current = motor.getStatorCurrent();
