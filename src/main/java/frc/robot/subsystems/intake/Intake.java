793f9b43fbac65f8143895583891d3bc1e67e6b6
Akshaya Gunnam <akshaya.gunnam@gmail.com>
2025-02-07 15:46:43 -0800

    ground intake v1

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -100,8 +100,8 @@ public class Intake extends SubsystemBase {
       rKV.initDefault(0.0002);
       rollerTargetAccelerationConfig.initDefault(0.0);
 
-      pKP.initDefault(.01);
-      pKD.initDefault(0);
+      pKP.initDefault(10);
+      pKD.initDefault(9);
       pKG.initDefault(0.0);
 
       pivotMaxVelocityConfig.initDefault(40);
