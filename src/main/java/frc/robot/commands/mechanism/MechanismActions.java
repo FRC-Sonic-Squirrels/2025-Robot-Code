a2ebe62046218c731d0791320cd07834af02fb5c
ViljamiBaker <viljamiolder@outlook.com>
2025-02-07 15:55:43 -0800

    comented out old functionality. Variables for the collision. some collision logic

--- src/main/java/frc/robot/commands/mechanism/MechanismActions.java
+++ src/main/java/frc/robot/commands/mechanism/MechanismActions.java
@@ -1,18 +1,18 @@
 package frc.robot.commands.mechanism;
 
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.team2930.LoggerEntry;
 import frc.lib.team2930.LoggerGroup;
 import frc.lib.team2930.TunableNumberGroup;
 import frc.lib.team6328.LoggedTunableNumber;
-import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
 import frc.robot.RobotStates.ScoringLevel;
 import frc.robot.commands.mechanism.MechanismPositions.MechanismPosition;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.intake.Intake;
 import java.util.function.Supplier;
 
 public class MechanismActions {
@@ -75,11 +75,58 @@ public class MechanismActions {
 
     var cmd =
         new Command() {
+          // make real
+          Intake intake = null;
+
           boolean elevatorInPosition = false;
           boolean armInPosition = false;
+          MechanismPosition targetPosition = position.get();
+          // the first 3 are the elevator, arm, and pivot
+          // 0,0 is the bottom of the elevator
+          // 1 unit is 1 inch
+          // TODO: get actual values and poses for colldiers
+          Translation2d[] colliders = {null, null, null, new Translation2d(0, 0)};
+          double[] radiii = {1.0, 1.0, 1.0, 1.0};
+          double SAFE_MULT = 1.2;
+
+          @Override
+          public void initialize() {}
 
           @Override
           public void execute() {
+            // update colliders for the moving parts
+            // elevator
+            colliders[0] = new Translation2d(0.0, elevator.getHeight().in(Units.Inches));
+            // arm, elevator plus arm stuff
+            colliders[1] =
+                colliders[0].plus(
+                    new Translation2d(ArmConstants.ARM_LENGTH.in(Units.Inches), 0)
+                        .rotateBy(arm.getAngle()));
+            // pivot
+            colliders[2] =
+                new Translation2d(ArmConstants.ARM_LENGTH.in(Units.Inches), 0)
+                    .rotateBy(intake.getPivotAngle());
+
+            for (int i = 0; i < 3; i++) { // loop over the moving colliders
+              for (int c = 0; c < colliders.length; c++) { // check against all other colliders
+                if (i == c) { // dont check if the other is myself
+                  continue;
+                }
+                Translation2d diff = colliders[i].minus(colliders[c]);
+                if (Math.pow(diff.getX(), 2) + Math.pow(diff.getY(), 2)
+                    < (radiii[i] + radiii[c])) { // circle col with raw radii
+                  // they hit eachother 😨
+                  continue;
+                } else if (Math.pow(diff.getX(), 2) + Math.pow(diff.getY(), 2)
+                    < (radiii[i] + radiii[c]) * SAFE_MULT) { // circle col with safety barrier
+                  // oh no they are getting very close
+                  continue;
+                }
+                arm.setAngle(targetPosition.armAngle());
+                elevator.setHeight(targetPosition.elevatorHeight());
+              }
+            }
+            /*
             MechanismPosition targetPosition = position.get();
             Distance safeHeight = Constants.ElevatorConstants.SAFE_HEIGHT;
             boolean runningArm =
@@ -118,6 +165,7 @@ public class MechanismActions {
             armInPosition =
                 arm.isAtTargetAngle(position.get().armAngle(), Rotation2d.fromDegrees(5.0));
             log_ArmInPosition.info(armInPosition);
+            */
           }
 
           @Override
