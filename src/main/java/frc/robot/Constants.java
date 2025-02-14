9b35682e100521f678bfa2ae77abd910a29308e9
Tim <timellis337@gmail.com>
2025-02-13 18:48:43 -0800

    add follower motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -317,7 +317,8 @@ public final class Constants {
 
     public static final int ARM_CAN_ID = 17;
 
-    public static final int ELEVATOR_CAN_ID = 37;
+    public static final int ELEVATOR_LEAD_CAN_ID = 37;
+    public static final int ELEVATOR_FOLLOW_CAN_ID = 38;
 
     public static final int END_EFFECTOR_CAN_ID = 30;
     public static final int END_EFFECTOR_SCORING_SIDE_TOF_CAN_ID = 39;
