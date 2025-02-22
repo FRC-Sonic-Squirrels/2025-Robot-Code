bf2a73b7bf4bd6eb08ef5a1e0fec94f1f0fb2b1b
Akshaya Gunnam <akshaya.gunnam@gmail.com>
2025-02-21 18:58:52 -0800

    reverted the changes that I did not make but it says I did

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -56,6 +56,7 @@ import frc.robot.commands.intake.IntakeSetPivotAngle;
 import frc.robot.commands.intake.IntakeSetRPM;
 import frc.robot.commands.intake.ScoreAlgae;
 import frc.robot.commands.mechanism.MechanismActions;
+import frc.robot.commands.mechanism.WaitUntilMovedDist;
 import frc.robot.configs.SimulatorRobotConfig;
 import frc.robot.subsystems.LED;
 import frc.robot.subsystems.LED.BaseRobotState;
@@ -138,6 +139,7 @@ public class RobotContainer {
   private final AutosManager autoManager;
 
   private Trigger gamepieceInRobot = new Trigger(() -> RobotStates.coralInRobot);
+  private Trigger gamepieceInEndEffector = new Trigger(() -> RobotStates.coralInEndEffector);
 
   public DigitalInput brakeModeButton = new DigitalInput(0);
   public DigitalInput zeroSensorsButton = new DigitalInput(1);
@@ -223,7 +225,7 @@ public class RobotContainer {
           new LED(
               () -> brakeModeTriggered,
               drivetrain::isGyroConnected,
-              () -> elevator.getHeight().in(Units.Inches) < 0.1);
+              () -> elevator.getHeight().in(Units.Inches) < 0.3);
     } else { // REAL and SIM robots HERE
       switch (robotType) {
         case ROBOT_SIMBOT_REAL_CAMERAS:
@@ -289,7 +291,7 @@ public class RobotContainer {
               new LED(
                   () -> brakeModeTriggered,
                   drivetrain::isGyroConnected,
-                  () -> elevator.getHeight().in(Units.Inches) > 0.1);
+                  () -> elevator.getHeight().in(Units.Inches) > 0.3);
           break;
 
         case ROBOT_2023_RETIRED_ROBER:
@@ -321,7 +323,7 @@ public class RobotContainer {
               new LED(
                   () -> brakeModeTriggered,
                   drivetrain::isGyroConnected,
-                  () -> elevator.getHeight().in(Units.Inches) > 0.1);
+                  () -> elevator.getHeight().in(Units.Inches) > 0.3);
           break;
 
         case ROBOT_2024_RETIRED_MAESTRO:
@@ -353,7 +355,7 @@ public class RobotContainer {
               new LED(
                   () -> brakeModeTriggered,
                   drivetrain::isGyroConnected,
-                  () -> elevator.getHeight().in(Units.Inches) > 0.1);
+                  () -> elevator.getHeight().in(Units.Inches) > 0.3);
           break;
 
         case ROBOT_2025_HOLO:
@@ -385,7 +387,7 @@ public class RobotContainer {
               new LED(
                   () -> brakeModeTriggered,
                   drivetrain::isGyroConnected,
-                  () -> elevator.getHeight().in(Units.Inches) > 0.1);
+                  () -> elevator.getHeight().in(Units.Inches) > 0.3);
           break;
 
         default:
@@ -416,7 +418,7 @@ public class RobotContainer {
               new LED(
                   () -> brakeModeTriggered,
                   drivetrain::isGyroConnected,
-                  () -> elevator.getHeight().in(Units.Inches) > 0.1);
+                  () -> elevator.getHeight().in(Units.Inches) > 0.3);
           break;
       }
     }
@@ -766,6 +768,14 @@ public class RobotContainer {
               new WheelRadiusCharacterization(
                   drivetrainWrapper, Constants.RobotMode.getRobot().config.get(), 0.5));
     }
+    // ---------- NON-CONTROLLER TRIGGERS
+
+    gamepieceInEndEffector.onTrue(
+        new WaitUntilMovedDist(drivetrainWrapper, Units.Meters.of(0.3))
+            .andThen(MechanismActions.scorePrepPosition(elevator, arm)));
+    gamepieceInEndEffector.onFalse(
+        new WaitUntilMovedDist(drivetrainWrapper, Units.Meters.of(0.3))
+            .andThen(MechanismActions.coralStationPosition(elevator, arm)));
 
     // ---------- ON-ROBOT CONTROLS ------------
 
@@ -997,6 +1007,9 @@ public class RobotContainer {
     resetSubsystems();
     vision.useMaxDistanceAwayFromExistingEstimate(false);
     vision.useGyroBasedFilteringForVision(false);
+
+    arm.setVoltage(0);
+    elevator.setPercentOut(0);
 
     is_teleop = false;
     is_autonomous = false;
