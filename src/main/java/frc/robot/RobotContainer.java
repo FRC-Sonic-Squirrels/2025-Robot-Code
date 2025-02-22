c17b98be95ad7ce365af54dc6e06633eae221b7d
ViljamiBaker <viljamiolder@outlook.com>
2025-02-21 20:09:39 -0800

    rebase

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -513,7 +513,7 @@ public class RobotContainer {
         .registerTrigger(XboxControllerWrapper.Button.rightBumper, "Intake")
         .whileTrue(
             CommandComposer.intakeCoralFromStation(
-                drivetrainWrapper, endEffector, elevator, arm, led, driverController));
+                drivetrainWrapper, endEffector, elevator, arm, led, driverController, true));
 
     driverController
         .registerTrigger(XboxControllerWrapper.Button.povLeft, "Score Algae")
