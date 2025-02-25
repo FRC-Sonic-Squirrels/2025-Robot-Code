1d89c12f083bf6798d1e1774367cd5d24a99e009
Sonic Squirrels Programming <sonicsquirrels@gmail.com>
2025-02-25 15:05:51 -0800

    Intake development

--- src/main/java/frc/robot/commands/intake/IntakeGround.java
+++ src/main/java/frc/robot/commands/intake/IntakeGround.java
@@ -1,9 +1,12 @@
 package frc.robot.commands.intake;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.team2930.LoggerEntry;
 import frc.lib.team2930.LoggerGroup;
+import frc.lib.team2930.TunableNumberGroup;
+import frc.lib.team6328.LoggedTunableNumber;
 import frc.robot.Constants.IntakeConstants.PivotConstants;
 import frc.robot.RobotStates;
 import frc.robot.subsystems.intake.Intake;
@@ -22,6 +25,11 @@ public class IntakeGround extends Command {
   private static final LoggerEntry.Bool logInputs_coralInIntake =
       logGroup.buildBoolean("coralInIntake");
 
+  private static final TunableNumberGroup group = new TunableNumberGroup("Intake Gamepiece");
+
+  private static final LoggedTunableNumber maximumPivotAngle =
+      group.build("maxPivotAngle", PivotConstants.MAX_PIVOT_ANGLE.getDegrees());
+
   /** Creates a new IntakeGround */
   public IntakeGround(Intake intake) {
 
@@ -34,7 +42,7 @@ public class IntakeGround extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intake.setPivotAngle(PivotConstants.MAX_PIVOT_ANGLE);
+    intake.setPivotAngle(Rotation2d.fromDegrees(maximumPivotAngle.get()));
     intake.setRollerVelocity(PivotConstants.GAMEPIECE_INTAKE_SPEED_RPM);
   }
 
--- src/main/java/frc/robot/commands/intake/ScoreAlgae.java
+++ src/main/java/frc/robot/commands/intake/ScoreAlgae.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.intake;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.team2930.TunableNumberGroup;
 import frc.lib.team6328.LoggedTunableNumber;
@@ -14,9 +15,13 @@ import frc.robot.subsystems.intake.Intake;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ScoreAlgae extends Command {
   /** Creates a new ScoreAlgae. */
+  // REMEMBER TO UPDATE CONSTANTS AFTER CHANGING TUNABLE NUMBERS! THESE ARE JUST HERE TO DEBUG AND
+  // TEST.
   private static final TunableNumberGroup group = new TunableNumberGroup("ScoreAlgae");
 
   private static final LoggedTunableNumber scoringVelocity = group.build("scoringVelocity", 400.0);
+  private static final LoggedTunableNumber algaeScoreAngle =
+      group.build("scoringAngle", PivotConstants.ALGAE_SCORE_ANGLE.getDegrees());
 
   private final Intake intake;
 
@@ -30,14 +35,14 @@ public class ScoreAlgae extends Command {
   @Override
   public void initialize() {
     if (RobotStates.algaeInRobot) {
-      intake.setPivotAngle(PivotConstants.ALGAE_SCORE_ANGLE);
+      intake.setPivotAngle(Rotation2d.fromDegrees(algaeScoreAngle.get()));
     }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (intake.isPivotAtTargetAngle(PivotConstants.ALGAE_SCORE_ANGLE)) {
+    if (intake.isPivotAtTargetAngle(Rotation2d.fromDegrees(algaeScoreAngle.get()))) {
       intake.setRollerVelocity(scoringVelocity.get());
     }
   }
