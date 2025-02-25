0612e5f99708920f0a00a8bdc3a6e1a44b5897a4
Tim <timellis337@gmail.com>
2025-02-24 19:20:34 -0800

    working sim

---
+++ src/main/java/frc/robot/commands/climber/Climb.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.climber;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.lib.team2930.LoggerEntry;
+import frc.lib.team2930.LoggerGroup;
+import frc.lib.team2930.TunableNumberGroup;
+import frc.lib.team6328.LoggedTunableNumber;
+import frc.robot.subsystems.climber.Climber;
+import java.util.function.BooleanSupplier;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Climb extends Command {
+
+  private final Climber climber;
+  private final BooleanSupplier confirmButton;
+  private boolean prevConfirm = true;
+  private int stage = 1;
+
+  private final TunableNumberGroup tunableGroup = new TunableNumberGroup("Climb");
+
+  private final LoggedTunableNumber firstAngle = tunableGroup.build("Stage1AngleRot", -1.5);
+  private final LoggedTunableNumber secondAngle = tunableGroup.build("Stage2AngleRot", 0);
+  private final LoggedTunableNumber thirdAngle = tunableGroup.build("Stage3AngleRot", -2.75);
+
+  private final LoggerGroup logGroup = LoggerGroup.build("Climb");
+  private final LoggerEntry.Bool logConfirm = logGroup.buildBoolean("Confirm");
+  private final LoggerEntry.Integer logStage = logGroup.buildInteger("Stage");
+
+  /** Creates a new Climb. */
+  public Climb(Climber climber, BooleanSupplier confirmButton) {
+    this.climber = climber;
+    this.confirmButton = confirmButton;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    boolean confirm = confirmButton.getAsBoolean();
+
+    if (confirm && !prevConfirm) {
+      stage++;
+    }
+
+    if (stage == 1) {
+      climber.setWinchAngle(Rotation2d.fromRotations(firstAngle.get()));
+    } else if (stage == 2) {
+      climber.setWinchAngle(Rotation2d.fromRotations(secondAngle.get()));
+    } else {
+      climber.setWinchAngle(Rotation2d.fromRotations(thirdAngle.get()));
+    }
+
+    logStage.info(stage);
+    logConfirm.info(confirm);
+    prevConfirm = confirm;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
