e5f2a9972d399ddaa79bdb0354b940323966fcd5
Tim <timellis337@gmail.com>
2025-02-26 19:11:27 -0800

    spotless

--- src/main/java/frc/robot/subsystems/swerve/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/swerve/Drivetrain.java
@@ -35,7 +35,6 @@ import frc.lib.team6328.LoggedTunableNumber;
 import frc.lib.team6328.PoseEstimator;
 import frc.lib.team6328.PoseEstimator.TimestampedVisionUpdate;
 import frc.robot.Constants;
-import frc.robot.Constants.RobotMode;
 import frc.robot.Robot;
 import frc.robot.configs.RobotConfig;
 import frc.robot.subsystems.swerve.gyro.GyroIO;
