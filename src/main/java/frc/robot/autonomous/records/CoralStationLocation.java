aaa95ea580480b1443ff530d3fe346587790a581
Davide Massarenti <davide@pulumi.com>
2025-03-09 09:13:51 -0700

    Let enums return their opposite side

--- src/main/java/frc/robot/autonomous/records/CoralStationLocation.java
+++ src/main/java/frc/robot/autonomous/records/CoralStationLocation.java
@@ -1,8 +1,28 @@
 package frc.robot.autonomous.records;
 
-public enum CoralStationLocation {
-  IA,
-  IB,
-  IC,
-  ID
+public enum CoralStationLocation implements OppositeSide<CoralStationLocation> {
+  IA {
+    @Override
+    public CoralStationLocation getOpposite() {
+      return ID;
+    }
+  },
+  IB {
+    @Override
+    public CoralStationLocation getOpposite() {
+      return IC;
+    }
+  },
+  IC {
+    @Override
+    public CoralStationLocation getOpposite() {
+      return IB;
+    }
+  },
+  ID {
+    @Override
+    public CoralStationLocation getOpposite() {
+      return IA;
+    }
+  };
 }
--- src/main/java/frc/robot/autonomous/records/ScoringLocation.java
+++ src/main/java/frc/robot/autonomous/records/ScoringLocation.java
@@ -3,18 +3,78 @@ package frc.robot.autonomous.records;
 import frc.robot.RobotStates.ScoringLevel;
 
 public record ScoringLocation(ReefSide side, ScoringLevel level) {
-  public enum ReefSide {
-    CA,
-    CB,
-    CC,
-    CD,
-    CE,
-    CF,
-    CG,
-    CH,
-    CI,
-    CJ,
-    CK,
-    CL
+  public enum ReefSide implements OppositeSide<ReefSide> {
+    CA {
+      @Override
+      public ReefSide getOpposite() {
+        return CB;
+      }
+    },
+    CB {
+      @Override
+      public ReefSide getOpposite() {
+        return CA;
+      }
+    },
+    CC {
+      @Override
+      public ReefSide getOpposite() {
+        return CL;
+      }
+    },
+    CD {
+      @Override
+      public ReefSide getOpposite() {
+        return CK;
+      }
+    },
+    CE {
+      @Override
+      public ReefSide getOpposite() {
+        return CJ;
+      }
+    },
+    CF {
+      @Override
+      public ReefSide getOpposite() {
+        return CI;
+      }
+    },
+    CG {
+      @Override
+      public ReefSide getOpposite() {
+        return CH;
+      }
+    },
+    CH {
+      @Override
+      public ReefSide getOpposite() {
+        return CG;
+      }
+    },
+    CI {
+      @Override
+      public ReefSide getOpposite() {
+        return CF;
+      }
+    },
+    CJ {
+      @Override
+      public ReefSide getOpposite() {
+        return CE;
+      }
+    },
+    CK {
+      @Override
+      public ReefSide getOpposite() {
+        return CD;
+      }
+    },
+    CL {
+      @Override
+      public ReefSide getOpposite() {
+        return CC;
+      }
+    }
   }
 }
