e969bdd48178c64db2263f672dc6d37f3b6f7b24
PsiFi96 <lt.theaker@icloud.com>
2025-04-17 15:08:12 -0700

    autoalliancecolor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -997,6 +997,8 @@ public class RobotContainer {
 
     SmartDashboard.putData("Confirm Auto", new RunsWhenDisabledInstantCommand(led::confirmAuto));
 
+    SmartDashboard.putString("Alliance Color", DriverStation.getAlliance().toString());
+
     SmartDashboard.putData(
         "Swerve Coast", new RunsWhenDisabledInstantCommand(() -> drivetrain.setBrakeMode(false)));
     SmartDashboard.putData(
